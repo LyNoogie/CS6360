5b8c733879807b12dde733a1fa67278ab0d111f8
Joshua Morrell <morrelljosh614@gmail.com>
2020-09-02 18:47:40 -0600

    section one scripts

---
+++ CS6260_Assignment1_1/Assets/RoomSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("2")){
+            this.transform.position = new Vector3(0.5f, 0.5f, 0.5f);
+            Debug.Log(this.transform.position);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+    }
+}
