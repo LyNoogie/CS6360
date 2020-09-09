b8355c141c0bde830c5814eea6f67b2fe5442fcf
Alec <alecnelson52@yahoo.com>
2020-09-09 13:43:32 -0600

    Orbit script

---
+++ CS6260_Assignment1_1/Assets/Oculus/VR/Prefabs/Orbit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Orbit : MonoBehaviour
+{
+    public Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0.0f, 0.5f, 0.0f, Space.Self);
+    }
+}
