3abb56fb0a6b999903166c846443dfdf3d10eef5
Alec <alecnelson52@yahoo.com>
2020-09-14 13:24:50 -0600

    Ball drop script

---
+++ CS6260_Assignment1_1/Assets/Scripts/BallDropScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDropScript : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public GameObject ball;
+
+    void Start() {
+        rigidbody = ball.AddComponent<Rigidbody>();
+        rigidbody.useGravity = false;
+    }
+
+
+    void OnTriggerEnter(Collider other) {
+        rigidbody.useGravity = true;
+    }
+}
