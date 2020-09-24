af58fad2a4d51c16164d48bd0388ff6cfb374b5b
u0940253 <u0940253@utah.edu>
2020-09-24 16:33:15 -0600

    add scripting pt 2

--- CS6260_Assignment1_2/Assets/light_game.cs
+++ CS6260_Assignment1_2/Assets/light_game.cs
@@ -1,18 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class light_game : MonoBehaviour
 {
+    private double time;
+    //public GameObject playerObj = null;
+    //private OVRPlayerController player;
+
+    public GameObject[] lightObjs;
+    public GameObject score_text_obj;
+    private float timer;
+    private int currentLight;
+    private int minIndex=0;
+    private int maxIndex=4;
+    private int score;
+    private Text score_text;
+    private Text instructions;
+   
+
+    public void updateScore()
+    {
+        score++;
+        Debug.Log("Score is " + score);
+        Debug.Log("current light is " + currentLight);
+        lightObjs[currentLight].GetComponent<Light>().enabled = false;
+        currentLight = Random.Range(minIndex, maxIndex);
+        lightObjs[currentLight].GetComponent<Light>().enabled = true;
+        timer = 0;
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        score_text= GameObject.FindGameObjectsWithTag("score_text")[0].GetComponent<Text>();
+        score_text.text = "0";
+        //player = playerObj.GetComponent<OVRPlayerController>();
+        score = 0;
+        lightObjs = GameObject.FindGameObjectsWithTag("trig_light");
+        Debug.Log(lightObjs.Length);
+
+        currentLight = Random.Range(minIndex, maxIndex);
+
+        for (int i = 0; i < maxIndex; i++)
+        {
+            lightObjs[i].GetComponent<Light>().enabled = false;
+        }
+        lightObjs[currentLight].GetComponent<Light>().enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > 3.0f)
+        {
+            lightObjs[currentLight].GetComponent<Light>().enabled = false;
+            currentLight = Random.Range(minIndex, maxIndex);
+            lightObjs[currentLight].GetComponent<Light>().enabled = true;
+            timer = 0;
+        }
+
     }
+
+  
 }
---
+++ CS6260_Assignment1_2/Assets/light_trigger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light_trigger : MonoBehaviour
+{
+    //public GameObject lo=null;
+    public Light light_t;
+    public light_game game;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        game = (light_game)FindObjectOfType(typeof(light_game));
+        if (game)
+        {
+            Debug.Log("found light game");
+
+        }
+        else
+        {
+            Debug.Log("light game not found");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider trig)
+    {
+        Debug.Log("entered Trigger");
+    }
+
+
+    void OnTriggerStay(Collider trig)
+    {
+        //var light = this.gameObject.GetComponent<Light>();
+        if (light_t != null)
+        {
+            if (light_t.enabled == true)
+            {
+                Debug.Log("Triggered on light!");
+                game.updateScore();
+                
+            }
+        }
+       
+    }
+}
