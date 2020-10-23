cad9a607a001f9ccafb1c0741201784fbb816a35
Alec <alecnelson52@yahoo.com>
2020-10-23 11:19:55 -0600

    Rotate vector field, calculate world space angles for flux vectors (not quite finished)

--- CS6360_final_project/AvalanceRescueSimulation/Assets/Oculus/VR/Prefabs/transmitting_script.cs
+++ CS6360_final_project/AvalanceRescueSimulation/Assets/Oculus/VR/Prefabs/transmitting_script.cs
@@ -51,19 +51,45 @@ public class transmitting_script : MonoBehaviour
         //float y = player.transform.position.y;
         //float z = player.transform.position.z;
         int min_index = GetClosestVector(player_trans.position.x, player_trans.position.y, player_trans.position.z);
-        //Debug.Log(min_index);
 
         var current_vec = vectors[min_index];
 
         float vx = current_vec.x;
-        float vy = current_vec.y;
+        float vz = current_vec.z;
+        double vector_degree;
 
-        double vector_degree = Math.Atan(vx / vy)*(180/Math.PI); //return degrees
+        //In calculations below, 0 should be replaced with worldspace angle/direction of player
+
+        // if vx, vz > 0, vector_degree = 0 + (90 - Math.Atan(vz / vx)*(180/Math.PI))
+        if (vx > 0 & vz > 0) 
+        {
+            vector_degree = 90 - Math.Atan(vz / vx)*(180/Math.PI);
+        }
+        // if vx > 0, vz < 0, vector_degree = 90 + (Math.Atan(vz / vx)*(180/Math.PI))
+        else if(vx > 0 & vz < 0)
+        {
+            vector_degree = 90 + (Math.Atan(vz / vx)*(180/Math.PI));
+        }
+        // if vx < 0, vz < 0, vector_degree = 180 + (90 - Math.Atan(vz / vx)*(180/Math.PI))
+        else if(vx < 0 & vz < 0)
+        {
+            vector_degree = 180 + (90 - Math.Atan(vz / vx)*(180/Math.PI));
+        }
+        // if vx < 0, vz > 0, vector_degree = 270 + (Math.Atan(vz / vx)*(180/Math.PI))
+        else 
+        {
+            vector_degree = 270 + (Math.Atan(vz / vx)*(180/Math.PI));
+        }
 
 
-        var y_rotation = player_trans.rotation.y;
-        Debug.Log(y_rotation);
+        // double vector_degree = Math.Atan(vz / vx)*(180/Math.PI); //return degrees
+
+        //Vector3 player_direction = player_trans.eulerAngles;
         Debug.Log(vector_degree);
+
+        // var y_rotation = player_trans.rotation.y;
+        // Debug.Log(y_rotation);
+        //Debug.Log(vector_degree);
 
 
         
@@ -78,7 +104,7 @@ public class transmitting_script : MonoBehaviour
             string[] items = line.Split(' ');
             float x = float.Parse(items[0]);
             float y = float.Parse(items[1]);
-            Vector3 t = new Vector3(x, y, 0.0f);
+            Vector3 t = new Vector3(x, 0.0f, y);
             coords.Add(t);
 
         }
@@ -91,7 +117,7 @@ public class transmitting_script : MonoBehaviour
             string[] items = line.Split(' ');
             float x = float.Parse(items[0]);
             float y = float.Parse(items[1]);
-            Vector3 t = new Vector3(x, y, 0.0f);
+            Vector3 t = new Vector3(x, 0.0f, y);
             vectors.Add(t);
 
         }
@@ -111,9 +137,15 @@ public class transmitting_script : MonoBehaviour
     {
         for (int i = 0; i < coords.Count; i++)
         {
-            Color color = new Color(0.0f, 0.0f, 1.0f);     
+            Color color;
             var start = coords[i];   
-            Debug.DrawLine(new Vector3(start.x, start.y , 0.0f), new Vector3(coords[i].x + vectors[i].x, coords[i].y + vectors[i].y, 0.0f) , color, 1000.0f);
+            if (start.x > 0) {
+                color = new Color(0.0f, 0.0f, 1.0f);     
+            }
+            else{
+                color = new Color(1.0f, 0.0f, 0.0f);  
+            }
+            Debug.DrawLine(new Vector3(start.x, 0.0f , start.z), new Vector3(coords[i].x + vectors[i].x, 0.0f, coords[i].z + vectors[i].z) , color, 1000.0f);
         }
     }
     
@@ -123,9 +155,9 @@ public class transmitting_script : MonoBehaviour
         int index = -1;
         for(int i=0; i < coords.Count; i++)
         {
-            if(Math.Sqrt(Math.Pow(x - coords[i].x, 2) + Math.Pow(y - coords[i].y, 2)) < min_dist)
+            if(Math.Sqrt(Math.Pow(x - coords[i].x, 2) + Math.Pow(z - coords[i].z, 2)) < min_dist)
             {
-                min_dist = Math.Sqrt(Math.Pow(x - coords[i].x, 2) + Math.Pow(y - coords[i].y, 2));
+                min_dist = Math.Sqrt(Math.Pow(x - coords[i].x, 2) + Math.Pow(z - coords[i].z, 2));
                 index = i;
             }
         }
