75e2c77a56248620cc75e7496e206ae7c8461321
u0940253 <u0940253@utah.edu>
2020-10-29 16:27:42 -0600

    update start scene

--- CS6360_final_project/AvalanceRescueSimulation/Assets/Scripts/initSettings.cs
+++ CS6360_final_project/AvalanceRescueSimulation/Assets/Scripts/initSettings.cs
@@ -8,10 +8,13 @@ public class initSettings : MonoBehaviour
 {
     public static int snow_amount = 0;
     public static bool hints = false;
-    public static float parameter = 0;
+    public static float snow_type = 0;
+    public static int level = 0;
 
     public Slider snowSlider;
-    public Slider otherSlider;
+    public Dropdown snowPackType;
+    public Dropdown difficulty;
+
 
     private string scene_name;
 
@@ -22,6 +25,16 @@ public class initSettings : MonoBehaviour
 
         if (scene_name != "StartMemu")
             Debug.Log("list init parameters: snow amount:" + snow_amount);
+
+        //difficulty = GameObject.Find("Scene_select_dropdown").GetComponent<Dropdown>();
+        difficulty.onValueChanged.AddListener(delegate {
+            Dropdown1ValueChanged(difficulty);
+        });
+
+        //snowPackType = GameObject.Find("Snow_pack_dropdown").GetComponent<Dropdown>();
+        snowPackType.onValueChanged.AddListener(delegate {
+            Dropdown2ValueChanged(snowPackType);
+        });
     }
 
 
@@ -31,12 +44,32 @@ public class initSettings : MonoBehaviour
         if (scene_name == "StartMemu")
         {
             snow_amount = (int)snowSlider.value;
-            parameter = otherSlider.value;
+            
         }
+
     }
+
+
 
     public void LoadScene(string scene_name)
     {
-        SceneManager.LoadScene(scene_name);
+
+        //set up player object
+
+
+        //todo:: add in scenes for difficulty
+        SceneManager.LoadScene("snowScene");
+    }
+    //Ouput the new value of the Dropdown into Text
+    void Dropdown1ValueChanged(Dropdown change)
+    {
+        level= change.value;
+        Debug.Log("level is "+ level.ToString());
+    }
+
+    void Dropdown2ValueChanged(Dropdown change)
+    {
+        snow_type = change.value;
+        Debug.Log("level is " + snow_type.ToString());
     }
 }
