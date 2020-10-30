4bbf60bfd9eec0b7ff815ad4b8a2755f1368809e
Alec <alecnelson52@yahoo.com>
2020-10-30 14:16:33 -0600

    kept Xuan's jitter fn

--- CS6360_final_project/AvalanceRescueSimulation/Assets/flashImage.cs
+++ CS6360_final_project/AvalanceRescueSimulation/Assets/flashImage.cs
@@ -8,16 +8,8 @@ using UnityEngine.UI;
 
 public class flashImage : MonoBehaviour
 {
-
     public Image img;
-<<<<<<< HEAD
-    RectTransform flashCanvasPos;
-    Vector3 backupFlashCanvasPos;
-    bool vibrating = false;
-
-=======
     public uint numOfStepPerJitter = 1; // increase for softer jitter
->>>>>>> b54f269bd0e510fcb44520a237858e70e7ca06df
 
     private float currentAlpha;
     private float flashAlhpaDeltaEachStep;
@@ -28,9 +20,6 @@ public class flashImage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        flashCanvasPos = GameObject.Find("Image").GetComponent<RectTransform>();
-        backupFlashCanvasPos = flashCanvasPos.localPosition;
-
         if (!img) Debug.Log("no imgae");
         currentAlpha = 0.0f;
         flashStepsToDo = 0;
@@ -49,24 +38,7 @@ public class flashImage : MonoBehaviour
         flashStepsOnMaxAlpha = (int)(maxAlpha / flashAlhpaDeltaEachStep);
         currentAlpha = 0.0f;
         flashStepsToDo = (int)(2 * maxAlpha / flashAlhpaDeltaEachStep);
-
-        Debug.Log("steps: "+ flashStepsToDo);
     }
-
-    public void Vibrate() {
-        float dx = 5 * UnityEngine.Mathf.Sin(10 * flashStepsToDo);
-        float dy = 5 * UnityEngine.Mathf.Cos( 8 * flashStepsToDo);
-        float xx = flashCanvasPos.localPosition.x;
-        float yy = flashCanvasPos.localPosition.y;
-        float zz = flashCanvasPos.localPosition.z;
-        flashCanvasPos.localPosition = new Vector3(xx + dx, yy + dy, zz);
-
-        if (flashStepsToDo == 0) {
-            vibrating = false;
-            flashCanvasPos.localPosition = backupFlashCanvasPos;
-        }
-    }
-
 
     // Update is called once per frame
     void Update()
@@ -75,11 +47,6 @@ public class flashImage : MonoBehaviour
         {
             print("k key was pressed");
             StartFlash(0.5f, 1);
-            vibrating = true;
-        }
-
-        if (vibrating) {
-            Vibrate();
         }
 
         if (flashStepsToDo != 0) {
