4b32cf7f659add0718b9eccd6bb05647aa3e7048
lynguyen <khanhly.t.nguyen@gmail.com>
2020-11-01 21:11:27 -0700

    Image added to the screen during successful probing

--- CS6360_final_project/AvalanceRescueSimulation/Assets/Scripts/Probe_Controller.cs
+++ CS6360_final_project/AvalanceRescueSimulation/Assets/Scripts/Probe_Controller.cs
@@ -10,6 +10,7 @@ public class Probe_Controller : MonoBehaviour
     float pPosZ;
     Vector3 burriedPos;
     bool withinRange;
+    flashImage fi;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,7 @@ public class Probe_Controller : MonoBehaviour
         probePos = new Vector3(player.position.x, player.transform.position.y, player.transform.position.z ) + player.transform.forward;
         this.transform.rotation = player.transform.rotation;
         withinRange = false;
+        fi = GameObject.Find("FlashCanvas").GetComponent<flashImage>();
     }
 
     // Update is called once per frame
@@ -32,6 +34,7 @@ public class Probe_Controller : MonoBehaviour
             {
                 // Add jitter effect here
                 Debug.Log("GOT IT");
+                fi.StartFlash(0.2f, 1);
             }
         }
     }
@@ -39,6 +42,6 @@ public class Probe_Controller : MonoBehaviour
     void CheckDistToSource(Vector3 probe)
     {
         double dist = Math.Sqrt(Math.Pow(probe.x - burriedPos.x, 2) + Math.Pow(probe.z - burriedPos.z, 2));
-        withinRange = dist < 5 ? true : false;
+        withinRange = dist < 50 ? true : false;
     }
 }
