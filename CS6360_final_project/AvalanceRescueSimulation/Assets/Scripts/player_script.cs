c770836bc383fe07f894adfda3b9437d6bfa980f
u0940253 <u0940253@utah.edu>
2020-11-02 09:13:08 -0700

    update player script for tool switching

--- CS6360_final_project/AvalanceRescueSimulation/Assets/Scripts/player_script.cs
+++ CS6360_final_project/AvalanceRescueSimulation/Assets/Scripts/player_script.cs
@@ -39,6 +39,11 @@ public class player_script : MonoBehaviour
 
 
 
+    private GameObject BeaconObj;
+    private GameObject ProbeObj;
+    private GameObject ShovelObj;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -47,6 +52,16 @@ public class player_script : MonoBehaviour
         CurrentTool = SAFTEY_TOOL.NONE;
         ShowFlux = false;
         load_initSetting = false;
+
+        VisibilityParam = 0;
+        LevelType = LEVEL.EASY;
+        CurrentTool = SAFTEY_TOOL.NONE;
+        ShowFlux = false;
+        BeaconObj = GameObject.Find("BeaconContainer");
+
+        //TODO:: uncomment when probe and shovel inported
+        //ProbeObj = GameObject.Find("ProbeContainer");
+        //ShovelObj = GameObject.Find("ShovelContainer");
     }
 
     // Update is called once per frame
@@ -83,6 +98,44 @@ public class player_script : MonoBehaviour
 
             // set bool so that it's executed once
             load_initSetting = true;
+        }
+        bool toggle_tool = false;
+
+        if (Input.GetJoystickNames().Length < 2)
+        {
+
+        }
+        else
+        {
+            toggle_tool = OVRInput.GetUp(OVRInput.Button.SecondaryThumbstick);
+        }
+        if (toggle_tool)
+        {
+            Debug.Log("TOggled");
+            //TODO:: Uncomment as things are filled in;
+            switch (CurrentTool)
+            {
+                case SAFTEY_TOOL.NONE:
+                    CurrentTool = SAFTEY_TOOL.BEACON;
+                    //ShovelObj.SetActive(false);
+                    break;
+                case SAFTEY_TOOL.BEACON:
+                    CurrentTool = SAFTEY_TOOL.PROBE;
+                    BeaconObj.SetActive(true);
+                    break;
+                case SAFTEY_TOOL.PROBE:
+                    CurrentTool = SAFTEY_TOOL.SHOVEL;
+                    BeaconObj.SetActive(false);
+                    //ProbeObj.SetActive(true);
+                    break;
+                case SAFTEY_TOOL.SHOVEL:
+                    CurrentTool = SAFTEY_TOOL.NONE;
+                    //ProbeObj.SetActive(false);
+                    //ShovelObj.SetActive(true);
+                    break;
+
+
+            }
         }
     }
 }
